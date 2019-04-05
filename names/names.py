3a5c84d2c722917df66dafbeeb8500e69d474145
CarloC24 <carlo.clamucha@gmail.com>
2019-04-05 12:13:28 -0500

    added names.py

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from lru_cache import LRUCache
 
 start_time = time.time()
 
@@ -10,13 +11,26 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+cache = LRUCache(20000)
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+
+def duplicatesfinder(names_1, names_2):
+    names_1 = list(set(names_1))
+    names_2 = list(set(names_2))
+    namesets = names_1 + names_2
+    for name in namesets:
+        if name in cache.cache:
+            print(name)
+            duplicatename = cache.get(name)
+            duplicates.append(duplicatename)
+        else:
+            cache.set(name, name)
+
+
+duplicatesfinder(names_1, names_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
